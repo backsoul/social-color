139d3d2942f408ed4bb8ce2f4da9134a5e70a18f
Daniel Sarmiento Dev <danielsarmientodeveloper@gmail.com>
2021-06-16 22:32:52 -0500

    implement vue router and composition api 3 in vue3 typescript

---
+++ socialnetwork/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <Layout />
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Layout from "@/layouts/default.vue";
+export default defineComponent({
+  components: { Layout },
+});
+</script>
---
+++ socialnetwork/src/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="w-screen h-screen">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Layout",
+});
+</script>
